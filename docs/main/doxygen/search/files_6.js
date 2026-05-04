f97ee27bf3aca313ecb32156532811c1224add37
kortemik <kortemik@users.noreply.github.com>
2026-05-04 05:02:34 +0000

    Deploying to gh-pages from @ teragrep/buf_01@9b8257c5172c60ffab3d0082dcbc7cc29bcd74b4 🚀

--- docs/main/doxygen/search/files_6.js
+++ docs/main/doxygen/search/files_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['trackedlease_2ejava_0',['TrackedLease.java',['../TrackedLease_8java.html',1,'']]],
-  ['trackedmemorysegmentlease_2ejava_1',['TrackedMemorySegmentLease.java',['../TrackedMemorySegmentLease_8java.html',1,'']]]
+  ['trackedleasemultiget_2ejava_1',['TrackedLeaseMultiGet.java',['../TrackedLeaseMultiGet_8java.html',1,'']]],
+  ['trackedmemorysegmentlease_2ejava_2',['TrackedMemorySegmentLease.java',['../TrackedMemorySegmentLease_8java.html',1,'']]]
 ];
--- docs/main/doxygen/search/variables_6.js
+++ docs/main/doxygen/search/variables_6.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['origin_0',['origin',['../classcom_1_1teragrep_1_1buf__01_1_1buffer_1_1lease_1_1TrackedMemorySegmentLease.html#a4e71fd31d5258e335ae2044aef57e18d',1,'com.teragrep.buf_01.buffer.lease.TrackedMemorySegmentLease.origin'],['../classcom_1_1teragrep_1_1buf__01_1_1buffer_1_1pool_1_1OpeningPool.html#ae994e09af070baafb376041bc59c56be',1,'com.teragrep.buf_01.buffer.pool.OpeningPool.origin']]]
+  ['origin_0',['origin',['../classcom_1_1teragrep_1_1buf__01_1_1buffer_1_1lease_1_1TrackedMemorySegmentLease.html#a4e71fd31d5258e335ae2044aef57e18d',1,'com.teragrep.buf_01.buffer.lease.TrackedMemorySegmentLease.origin'],['../classcom_1_1teragrep_1_1buf__01_1_1buffer_1_1pool_1_1OpeningPool.html#ae994e09af070baafb376041bc59c56be',1,'com.teragrep.buf_01.buffer.pool.OpeningPool.origin'],['../classcom_1_1teragrep_1_1buf__01_1_1buffer_1_1pool_1_1TrackedLeaseMultiGet.html#a7469a7dd88f0f94b4f34d67792f82b91',1,'com.teragrep.buf_01.buffer.pool.TrackedLeaseMultiGet.origin']]]
 ];
